2aac3c766013e0043eda24ab6d65374acc7cbbbe
Risharth Pardeshi <risharthpardeshi@gmail.com>
2023-06-20 22:29:45 +0530

    Add files via upload

---
+++ 0003LongestSubstrWithoutRepeatingCharacters.h
@@ -0,0 +1,34 @@
+/*
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: "abcabcbb"
+Output: 3 
+Explanation: The answer is "abc", with the length of 3. 
+Example 2:
+
+Input: "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+*/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+    
+        unordered_map<int,int> m;
+        int start=0,res=0;
+        for(int i=0;i<s.size();i++){
+            //更新start
+            if(m.count(s[i])!=0)
+                start=max(start,m[s[i]]+1);
+            //更新map
+            m[s[i]]=i;
+            //更新res
+            res=max(res,i-start+1);
+        }
+        return res;
+    }
+};
